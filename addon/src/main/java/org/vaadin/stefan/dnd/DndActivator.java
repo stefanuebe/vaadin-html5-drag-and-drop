4e0cd01ce22afdf80e294689c3346309e4397d47
Stefan Uebe <stefan@vaadin.com>
2018-08-24 09:15:29 +0200

    integrated polyfil, but still WIP

---
+++ addon/src/main/java/org/vaadin/stefan/dnd/DndActivator.java
@@ -0,0 +1,28 @@
+package org.vaadin.stefan.dnd;
+
+import com.vaadin.flow.component.HasElement;
+import com.vaadin.flow.component.UI;
+import com.vaadin.flow.component.dependency.JavaScript;
+import com.vaadin.flow.component.dependency.StyleSheet;
+import com.vaadin.flow.component.page.Page;
+
+public class DndActivator {
+	public static final void activateMobileDnd(HasElement component) {
+		component.getElement().getNode().runWhenAttached(DndActivator::activateMobileDnd);
+	}
+	public static final void activateMobileDnd(UI ui) {
+		Page page = ui.getPage();
+		page.addStyleSheet("frontend://bower_components/mobile-drag-drop/release/default.css");
+		page.addJavaScript("frontend://bower_components/mobile-drag-drop/release/index.min.js");
+		page.addJavaScript("frontend://bower_components/mobile-drag-drop/release/scroll-behaviour.min.js");
+
+		page.executeJavaScript("var polyfillApplied = MobileDragDrop.polyfill({" +
+				"            dragImageTranslateOverride: MobileDragDrop.scrollBehaviourDragImageTranslateOverride," +
+				"});" +
+				"if (polyfillApplied) {" +
+				"   alert('polyfill applied');" +
+				"}");
+
+
+	}
+}
--- demo/src/main/java/org/vaadin/stefan/dnd/demo/DragAndDropView.java
+++ demo/src/main/java/org/vaadin/stefan/dnd/demo/DragAndDropView.java
@@ -1,13 +1,17 @@
 package org.vaadin.stefan.dnd.demo;
 
 import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.checkbox.Checkbox;
 import com.vaadin.flow.component.dependency.HtmlImport;
+import com.vaadin.flow.component.dependency.JavaScript;
+import com.vaadin.flow.component.dependency.StyleSheet;
 import com.vaadin.flow.component.html.Span;
 import com.vaadin.flow.component.notification.Notification;
 import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.component.textfield.TextField;
 import com.vaadin.flow.router.Route;
+import org.vaadin.stefan.dnd.DndActivator;
 import org.vaadin.stefan.dnd.drag.DragSourceExtension;
 import org.vaadin.stefan.dnd.drop.DropTargetExtension;
 
@@ -20,25 +24,31 @@ public class DragAndDropView extends VerticalLayout {
 
 	public static final int DURATION = 1000;
 	public static final Notification.Position POSITION = Notification.Position.TOP_END;
+	private final Checkbox showEvents;
 
 	public DragAndDropView() {
+		DndActivator.activateMobileDnd(this);
+
+		showEvents = new Checkbox("Show events", true);
+		add(showEvents);
+
 		TestComponent d1 = new TestComponent(1);
 		TestComponent d2 = new TestComponent(2);
 
 		DragSourceExtension<TestComponent> extend1 = DragSourceExtension.extend(d1);
 		DragSourceExtension<TestComponent> extend2 = DragSourceExtension.extend(d2);
 
-		extend1.addDragStartListener(event -> Notification.show("Event dragstart for 1", DURATION, POSITION));
-		extend2.addDragStartListener(event -> Notification.show("Event dragstart for 2", DURATION, POSITION));
+		extend1.addDragStartListener(event -> showEventNotification("Event dragstart for 1"));
+		extend2.addDragStartListener(event -> showEventNotification("Event dragstart for 2"));
 
 		VerticalLayout dropTarget = new VerticalLayout();
 		dropTarget.add(new Span("Outer drop target"));
 		dropTarget.addClassName("outer-drop");
 
 		DropTargetExtension<VerticalLayout> dropTargetExtension = DropTargetExtension.extend(dropTarget);
-		dropTargetExtension.addDragEnterListener(event -> Notification.show("Event dragenter outer", DURATION, POSITION));
-		dropTargetExtension.addDragLeaveListener(event -> Notification.show("Event dragleave outer", DURATION, POSITION));
-		dropTargetExtension.addDropListener(event -> Notification.show("Event drop outer", DURATION, POSITION));
+		dropTargetExtension.addDragEnterListener(event -> showEventNotification("Event dragenter outer"));
+		dropTargetExtension.addDragLeaveListener(event -> showEventNotification("Event dragleave outer"));
+		dropTargetExtension.addDropListener(event -> showEventNotification("Event drop outer"));
 
 		add(d1, d2, dropTarget);
 
@@ -47,13 +57,19 @@ public class DragAndDropView extends VerticalLayout {
 		innerDropTarget.addClassName("inner-drop");
 
 		DropTargetExtension<VerticalLayout> innerDropTargetExtension = DropTargetExtension.extend(innerDropTarget);
-		innerDropTargetExtension.addDragEnterListener(event -> Notification.show("Event dragenter inner", DURATION, POSITION));
-		innerDropTargetExtension.addDragLeaveListener(event -> Notification.show("Event dragleave inner", DURATION, POSITION));
-		innerDropTargetExtension.addDropListener(event -> Notification.show("Event drop inner", DURATION, POSITION));
+		innerDropTargetExtension.addDragEnterListener(event -> showEventNotification("Event dragenter inner"));
+		innerDropTargetExtension.addDragLeaveListener(event -> showEventNotification("Event dragleave inner"));
+		innerDropTargetExtension.addDropListener(event -> showEventNotification("Event drop inner"));
 
 		dropTarget.add(innerDropTarget);
 
 		setClassName("main-layout");
+	}
+
+	private void showEventNotification(String s) {
+		if(showEvents.getValue()) {
+			Notification.show(s, DURATION, POSITION);
+		}
 	}
 
 	public static class TestComponent extends HorizontalLayout {
