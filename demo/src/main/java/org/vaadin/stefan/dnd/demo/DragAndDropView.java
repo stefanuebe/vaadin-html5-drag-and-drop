11f6c279ebb8e5d1fca8df4b2a88b8e0cec63a2e
Stefan Uebe <stefan@vaadin.com>
2018-08-31 10:11:25 +0200

    comments

--- demo/src/main/java/org/vaadin/stefan/dnd/demo/DragAndDropView.java
+++ demo/src/main/java/org/vaadin/stefan/dnd/demo/DragAndDropView.java
@@ -1,11 +1,9 @@
 package org.vaadin.stefan.dnd.demo;
 
-import com.vaadin.flow.component.UI;
+import com.vaadin.flow.component.Component;
 import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.checkbox.Checkbox;
 import com.vaadin.flow.component.dependency.HtmlImport;
-import com.vaadin.flow.component.dependency.JavaScript;
-import com.vaadin.flow.component.dependency.StyleSheet;
 import com.vaadin.flow.component.html.Span;
 import com.vaadin.flow.component.notification.Notification;
 import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
@@ -28,32 +26,36 @@ public class DragAndDropView extends VerticalLayout {
 	private final Checkbox showEvents;
 
 	public DragAndDropView() {
-		DndActivator.activateMobileDnd();
-
 		showEvents = new Checkbox("Show events", true);
 		add(showEvents);
 
-		TestComponent d1 = new TestComponent(1);
-		TestComponent d2 = new TestComponent(2);
+		// --- activate polyfill
+		DndActivator.activateMobileDnd();
 
-		DragSourceExtension<TestComponent> extend1 = DragSourceExtension.extend(d1);
-		DragSourceExtension<TestComponent> extend2 = DragSourceExtension.extend(d2);
+		// --- define dnd components
+		Component draggable1 = new DragTestComponent(1);
+		Component draggable2 = new DragTestComponent(2);
+
+		VerticalLayout dropTarget = new VerticalLayout();
+		dropTarget.add(new Span("Outer drop target"));
+		dropTarget.addClassName("outer-drop");
+
+		add(draggable1, draggable2, dropTarget);
+
+		// --- define drag extension
+		DragSourceExtension<Component> extend1 = DragSourceExtension.extend(draggable1);
+		DragSourceExtension<Component> extend2 = DragSourceExtension.extend(draggable2);
 
 		extend1.addDragStartListener(event -> showEventNotification("Event dragstart for 1"));
 		extend2.addDragStartListener(event -> showEventNotification("Event dragstart for 2"));
 		extend1.addDragEndListener(event -> showEventNotification("Event dragend for 1"));
 		extend2.addDragEndListener(event -> showEventNotification("Event dragend for 2"));
 
-		VerticalLayout dropTarget = new VerticalLayout();
-		dropTarget.add(new Span("Outer drop target"));
-		dropTarget.addClassName("outer-drop");
-
+		// --- define drop extension
 		DropTargetExtension<VerticalLayout> dropTargetExtension = DropTargetExtension.extend(dropTarget);
 		dropTargetExtension.addDragEnterListener(event -> showEventNotification("Event dragenter outer"));
 		dropTargetExtension.addDragLeaveListener(event -> showEventNotification("Event dragleave outer"));
 		dropTargetExtension.addDropListener(event -> showEventNotification("Event drop outer"));
-
-		add(d1, d2, dropTarget);
 
 		VerticalLayout innerDropTarget = new VerticalLayout();
 		innerDropTarget.add(new Span("Inner drop target"));
@@ -86,8 +88,8 @@ public class DragAndDropView extends VerticalLayout {
 		}
 	}
 
-	public static class TestComponent extends HorizontalLayout {
-		public TestComponent(int i) {
+	public static class DragTestComponent extends HorizontalLayout {
+		public DragTestComponent(int i) {
 			addClassName("drag-source");
 			add(new Span("Dragabble " + i));
 			add(new Button("My Button " + i));
